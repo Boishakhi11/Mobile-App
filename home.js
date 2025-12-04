e671f00bf46efa272678d9a5a3d4ffbf93c5ca40
Boishakhi G <bgmukta11@gmail.com>
2025-12-04 01:03:12 +0100

    completed with validation

--- home.js
+++ home.js
@@ -1,5 +1,6 @@
 //const variable for pin number
  const pinNumber = 1234;
+ const transactionHistory = [];
 
  //function to get value
  function getValue(id) {
@@ -47,6 +48,12 @@
     .classList.add('border-blue-900', 'bg-blue-100', 'border-2')
  }
 
+ //logout feature
+ document.getElementById('logout').addEventListener('click', function(e){
+    e.preventDefault();
+    window.location.href = 'index.html';
+ })
+
 
 //add money feature
 document.getElementById('add-money-btn')
@@ -56,12 +63,16 @@ document.getElementById('add-money-btn')
             const bankName = getValue('bank-name');
             const accountNumber = getValue('Account-number');
             const amount = convertValue('Amount');
+
+            if (amount <= 0) {
+                alert("Inavlid Amount");
+                return;
+            }
+
             const pin = convertValue('pin');
             const availableBalance = convertInnertext('available-balance');
 
             const totalAvailableBalance = availableBalance + amount;
-
-    
 
             if ( pin !== pinNumber) {
                 alert('Please provide vallid pin number');
@@ -72,19 +83,37 @@ document.getElementById('add-money-btn')
             document.getElementById('available-balance').innerText = totalAvailableBalance;
               alert ('Money Added Succesfully');
                 }
+
+            const history = {
+                name : 'Add Money',
+                date : new Date().toLocaleTimeString()
+            }
+
+            transactionHistory.push(history);
+            console.log(transactionHistory);
+
+            // Clear Add Money Form
+            document.getElementById("add-money-form-container").reset();
             })
 
+           
 
 //cash out feature
 document.getElementById('withdraw-money-btn')
 .addEventListener('click', function(e) {
     e.preventDefault()
     const agentNumber = getValue('Agent-number')
-    const amountToWithdraw = convertValue('Cash-out-amount') ;
+    const amountToWithdraw = convertValue('Cash-out-amount');
     const pinCode = convertValue('pin-code');
     const availableBalance = convertInnertext('available-balance');
 
     const totalAvailableBalance = availableBalance - amountToWithdraw;
+
+    if (amountToWithdraw <= 0 || amountToWithdraw > availableBalance)
+    {
+        alert('Invalid Amount')
+        return;
+    }
 
     if(agentNumber.length < 11) {
         alert('Please provide valid agent number');
@@ -99,6 +128,17 @@ document.getElementById('withdraw-money-btn')
         document.getElementById('available-balance').innerText = totalAvailableBalance;
         alert('Cash Out Successful')
     }
+
+    const history = {
+                name : 'CashOut',
+                date : new Date().toLocaleTimeString()
+            }
+
+            transactionHistory.push(history);
+            console.log(transactionHistory);
+
+    //clear cash out form
+    document.getElementById('cash-out-form-container').reset();
 
 })
 
@@ -115,6 +155,12 @@ document.getElementById('transfer-money-btn')
 
     const totalAvailableBalance = availableBalance - transferAmount;
 
+    if(transferAmount <= 0 || transferAmount > availableBalance)
+    {
+        alert('Invalid Amount')
+        return;
+    }
+
     if(recieverNumber.length < 11){
         alert('Please Provide a Valid Account Number');
         return;
@@ -129,6 +175,16 @@ document.getElementById('transfer-money-btn')
          document.getElementById('available-balance').innerText = totalAvailableBalance;
          alert ("Money Succesfully Transferred");
     }
+
+    const history = {
+                name : 'Transfer Money',
+                date : new Date().toLocaleTimeString()
+            }
+
+            transactionHistory.push(history);
+            console.log(transactionHistory);
+
+    document.getElementById('transfer-money-form-container').reset();
 })
 
 
@@ -145,6 +201,12 @@ document.getElementById('pay-now-btn')
 
     const totalAvailableBalance = availableBalance - amountToPay;
 
+    if (amountToPay <= 0 || amountToPay > availableBalance)
+    {
+        alert('Invalid Amount')
+        return;
+    }
+
     if(billAcountNumber.length < 11){
         alert('Please Provide a Valid Account Number');
         return;
@@ -159,9 +221,44 @@ document.getElementById('pay-now-btn')
          document.getElementById('available-balance').innerText = totalAvailableBalance;
          alert ("Bill paid succesfully");
     }
-    console.log(bankName,billAcountNumber, amountToPay, pin);
+    
+    const history = {
+                name : 'Pay Bill',
+                date : new Date().toLocaleTimeString()
+            }
+
+            transactionHistory.push(history);
+            console.log(transactionHistory);
+
+    document.getElementById('pay-bill-form-container').reset();
+
 })
 
+
+//transaction history feature
+document.getElementById('transaction-toggole')
+.addEventListener('click', function(e){
+    e.preventDefault();
+
+    const transactionContainer = document.getElementById('transaction-history')
+
+    for (const history of transactionHistory){
+        const div = document.createElement('div')
+        div.innerHTML = ` <div class="flex justify-between items-center">
+        <div class="flex gap-5 items-center mt-3">
+            <img class="rounded-md h-8" src="./assets/wallet1.png">
+            <div class="flex flex-col gap-1">
+                <h1>${history.name}</h1>
+                <p class="text-[#08080880]">${history.date}</p>
+            </div>
+        </div>
+            <i class="fa-solid fa-ellipsis-vertical"></i>
+        </div>
+    </div>`
+
+    transactionContainer.appendChild(div);
+    }
+})
 
 document.getElementById('add-money-toggole')
 .addEventListener('click', function() {
@@ -195,7 +292,7 @@ document.getElementById('pay-bill-toogole')
 
 document.getElementById('transaction-toggole')
 .addEventListener('click', function(){
-    handleToggle('latest-payment-form');
+    handleToggle('transaction-history-form');
     handleToggleColor('transaction-toggole')
 })
 
